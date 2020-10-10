93884847ef09dc3f84a45e980a27c0b110bc987a
Uy Ha <hchanuy@gmail.com>
2020-10-10 17:34:05 +0200

    Fix small mistakes: - Required python too new, switch to 3.6 - Get the correct download status

--- cmake/FindConan.cmake
+++ cmake/FindConan.cmake
@@ -16,8 +16,9 @@ if (Conan_EXECUTABLE)
             file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake"
                     "${arg_DIR}/conan.cmake"
                     STATUS download_status)
-            list(POP_BACK download_status download_error)
+            list(POP_FRONT download_status download_error)
             if (NOT download_error EQUAL 0)
+                file(REMOVE ${arg_DIR}/conan.cmake)
                 message(FATAL_ERROR "Failed to download conan.cmake")
             endif ()
         endif ()
