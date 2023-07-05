424bf51a17e04e152d62a58b3e29d3368eefd39d
Uy Ha <hchanuy@gmail.com>
2023-07-05 20:52:58 +0000

    have dependencies listed on seperated lines

--- src/conanfile.py
+++ src/conanfile.py
@@ -9,7 +9,9 @@ class ProjectName(ConanFile):
     generators = "CMakeDeps"
 
     def requirements(self):
-        dependencies = ["fmt/9.1.0"]
+        dependencies = [
+            "fmt/9.1.0",
+        ]
 
         for dep in dependencies:
             self.requires(dep)
--- tests/conanfile.py
+++ tests/conanfile.py
@@ -9,7 +9,10 @@ class ProjectNameTest(ConanFile):
     generators = "CMakeDeps"
 
     def requirements(self):
-        dependencies = ["fmt/9.1.0", "catch2/3.3.2"]
+        dependencies = [
+            "fmt/9.1.0",
+            "catch2/3.3.2",
+        ]
 
         for dep in dependencies:
             self.requires(dep)
