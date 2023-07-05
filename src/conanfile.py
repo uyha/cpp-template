e78f5719df0f664c1b59c90c83250486c16bb551
Uy Ha <hchanuy@gmail.com>
2023-07-05 20:52:18 +0000

    disable noisy pylint

--- src/conanfile.py
+++ src/conanfile.py
@@ -1,4 +1,7 @@
-from conan import ConanFile
+# pylint: disable=missing-class-docstring, too-few-public-methods,
+# pylint: disable=missing-function-docstring, missing-module-docstring,
+
+from conan import ConanFile  # pylint: disable=import-error
 
 
 class ProjectName(ConanFile):
